9bacdae2556fa2697ae8ebd9616bce329a651d87
BoYanZh <bz14520@uga.edu>
2024-03-17 00:24:42 -0400

    feat: send email (#1)

--- pkg/config/root.go
+++ pkg/config/root.go
@@ -20,6 +20,13 @@ type conf struct {
 	// jwt config
 	JWTSecret        string `env:"JWT_SECRET"                 envDefault:"secret"`
 	JWTExpireSeconds int64  `env:"JWT_EXPIRE_SECONDS"         envDefault:"1209600"` // 14 days, in seconds
+
+	// email config
+	EmailUsername string `env:"EMAIL_USERNAME" envDefault:"api"`
+	EmailPassword string `env:"EMAIL_PASSWORD" envDefault:"password"`
+	EmailSMTPHost string `env:"EMAIL_SMTP_HOST" envDefault:"live.smtp.mailtrap.io"`
+	EmailSMTPPort int    `env:"EMAIL_SMTP_PORT" envDefault:"587"`
+	EmailFrom     string `env:"EMAIL_FROM" envDefault:""`
 }
 
 var Conf *conf
