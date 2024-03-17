f2ad85ae2642f772d170026bb21cbbec3b9f63ac
vrthelen <vrt92429@uga.edu>
2024-03-17 18:20:06 -0400

    adding structs

---
+++ app/model/cinema.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// User struct to describe User object.
+type Cinema struct {
+	ID               uuid.UUID `gorm:"primarykey;type:uuid;default:(uuid_generate_v4())"`
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	DeletedAt        gorm.DeletedAt `gorm:"index"`
+	Name             string
+	Location         string
+	MovieList        []Movie
+	BookingList      []Booking
+}
