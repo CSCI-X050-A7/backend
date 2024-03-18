976632165c107f2e44ffade5e4539fb78a6af574
thelenvr <156618549+thelenvr@users.noreply.github.com>
2024-03-17 20:17:26 -0400

    feat: adding structs (#3)
    Co-authored-by: vrthelen <vrt92429@uga.edu>
    Co-authored-by: Boming Zhang <bz14520@uga.edu>

---
+++ app/model/promotion.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// User struct to describe User object.
+type Promotion struct {
+	ID             uuid.UUID `gorm:"primarykey;type:uuid;default:(uuid_generate_v4())"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      gorm.DeletedAt `gorm:"index"`
+	IsExpired      bool
+	CardNumber     string
+	ExpiryDate     string
+	BillingAddress string
+}
---
+++ app/model/ticket.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// User struct to describe User object.
+type Ticket struct {
+	ID        uuid.UUID `gorm:"primarykey;type:uuid;default:(uuid_generate_v4())"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Seat      int
+	Show      string
+	Type      string
+	Price     float64
+}
