629ea12b511922e246bb93252b087744a5f82632
Boming Zhang <bz14520@uga.edu>
2024-04-16 15:02:05 -0400

    feat: schema

--- app/controller/order.go
+++ app/controller/order.go
@@ -10,7 +10,7 @@ import (
 //	@Tags		Order
 //	@Accept		json
 //	@Produce	json
-//	@Success	200	{object}	interface{}
+//	@Success	200	{object}	schema.Order
 //	@Router		/api/v1/order/{id} [get]
 func GetOrder(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{})
--- app/schema/order.go
+++ app/schema/order.go
@@ -5,6 +5,7 @@ import "github.com/google/uuid"
 type Order struct {
 	ID uuid.UUID `json:"id" validate:"required"`
 	// TODO: more fields
+	Field string `json:"field" validate:"required"`
 }
 
 type OrderListResponse = ListResponse[Order]
