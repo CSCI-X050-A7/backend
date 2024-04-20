443cdfe682790fc060fb0e397b5d52d1a1321b1b
thelenvr <thelenvr@gmail.com>
2024-04-20 19:35:24 -0400

    feat: promo endpoint

---
+++ pkg/route/promotion.go
@@ -0,0 +1,16 @@
+package route
+
+import (
+	"github.com/CSCI-X050-A7/backend/app/controller"
+	"github.com/CSCI-X050-A7/backend/pkg/middleware"
+	"github.com/gofiber/fiber/v2"
+)
+
+func PromoRoutes(a *fiber.App) {
+	route := a.Group("/api/v1/promotions")
+	route.Get("/", controller.GetPromos)
+	routeProtectedAdmin := route.Group("", middleware.JWTProtected(), middleware.IsAdmin)
+	routeProtectedAdmin.Post("/", controller.CreatePromo)
+	routeProtectedAdmin.Put("/:id", controller.UpdatePromo)
+	//routeProtectedAdmin.Delete("/:id", controller.DeletePromo)
+}
