936aeaa668f46d48e933d3ebdd88262f9aef6cd7
pdp32147 <pdp32147@uga.edu>
2024-04-22 03:24:44 -0400

    create new order and show models

---
+++ app/model/order.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// Show struct to describe order object.
+type Order struct {
+	ID           uuid.UUID `gorm:"primarykey;type:uuid;default:(uuid_generate_v4())"`
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	DeletedAt    gorm.DeletedAt `gorm:"index"`
+	TicketsArray string         //replace with Ticket[] once that struct is created
+	Promotion    Promotion
+	Show         Show
+	Card         string //replace with Card once that struct is created
+}
---
+++ app/model/show.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// Show  struct to describe show object.
+type Show struct {
+	ID              uuid.UUID `gorm:"primarykey;type:uuid;default:(uuid_generate_v4())"`
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+	DeletedAt       gorm.DeletedAt `gorm:"index"`
+	Movie           Movie
+	StartTime       time.Time
+	EndTime         time.Time
+	BookingFee      float32
+	TicketPrice     float32
+	TicketType      string
+	Theaterlocation string
+}
