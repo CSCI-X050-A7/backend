e75e4849fa06156ffa582ca38db5bb2327c4cd63
thelenvr <thelenvr@gmail.com>
2024-04-24 11:37:21 -0400

    add newline + api doc

--- app/controller/card.go
+++ app/controller/card.go
@@ -8,6 +8,17 @@ import (
 	"github.com/google/uuid"
 )
 
+// GetCard func gets a card.
+//
+// @Description   a payment card.
+// @Summary       get a payment card
+// @Tags          Card
+// @Accept        json
+// @Produce 	  json
+// @Param		  id   path	    string	true	"Card ID"
+// @Success       200  {object}	    schema.Card
+// @Failure		  400, 404 {object} schema.ErrorResponse "Error"
+// @Router        /api/v1/cards/{id} [get]
 func GetCard(c *fiber.Ctx) error {
 	ID, err := uuid.Parse(c.Params("id"))
 	if err != nil {
