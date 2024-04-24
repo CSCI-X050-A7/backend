04624849db1af483fe4ce2be22ec2cf8685afb69
pdp32147 <pdp32147@uga.edu>
2024-04-24 00:19:32 -0400

    Populated controller files

--- app/model/show.go
+++ app/model/show.go
@@ -9,15 +9,17 @@ import (
 
 // Show  struct to describe show object.
 type Show struct {
-	ID              uuid.UUID `gorm:"primarykey;type:uuid;default:(uuid_generate_v4())"`
-	CreatedAt       time.Time
-	UpdatedAt       time.Time
-	DeletedAt       gorm.DeletedAt `gorm:"index"`
-	Movie           Movie
-	StartTime       time.Time
-	EndTime         time.Time
-	BookingFee      float32
-	TicketPrice     float32
-	TicketType      string
-	Theaterlocation string
+	ID                uuid.UUID `gorm:"primarykey;type:uuid;default:(uuid_generate_v4())"`
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
+	DeletedAt         gorm.DeletedAt `gorm:"index"`
+	Movie             Movie
+	StartTime         time.Time
+	EndTime           time.Time
+	BookingFee        float32
+	AdultTicketPrice  float32
+	ChildTicketPrice  float32
+	SeniorTicketPrice float32
+	TicketType        string
+	TheaterLocation   string
 }
