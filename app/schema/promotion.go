c4ba32c5920b5d84071a9aca2ae7ecf79c56fb9e
thelenvr <thelenvr@gmail.com>
2024-05-04 15:23:58 -0400

    removed unnecessary fields

--- app/schema/promotion.go
+++ app/schema/promotion.go
@@ -6,22 +6,16 @@ import (
 
 // Promotion struct to describe promotion object.
 type Promotion struct {
-	ID            uuid.UUID `json:"id" validate:"required"`
-	IsExpired     bool      `json:"is_expired" validate:"required"`
-	ExpiryDate    string    `json:"expiry_date" validate:"required,lte=255"`
-	MovieAffected string    `json:"movie_affected" validate:"required,lte=255"`
-	Discount      float64   `json:"discount" validate:"required"`
-	Title         string    `json:"title" validate:"required,lte=255"`
-	Description   string    `json:"description" validate:"required,lte=255"`
+	ID         uuid.UUID `json:"id" validate:"required"`
+	IsExpired  bool      `json:"is_expired" validate:"required"`
+	ExpiryDate string    `json:"expiry_date" validate:"required,lte=255"`
+	Discount   float64   `json:"discount" validate:"required"`
 }
 
 type UpsertPromotion struct {
-	IsExpired     bool    `json:"is_expired" validate:"required"`
-	ExpiryDate    string  `json:"expiry_date" validate:"required,lte=255"`
-	MovieAffected string  `json:"movie_affected" validate:"required,lte=255"`
-	Discount      float64 `json:"discount" validate:"required"`
-	Title         string  `json:"title" validate:"required,lte=255"`
-	Description   string  `json:"description" validate:"required,lte=255"`
+	IsExpired  bool    `json:"is_expired" validate:"required"`
+	ExpiryDate string  `json:"expiry_date" validate:"required,lte=255"`
+	Discount   float64 `json:"discount" validate:"required"`
 }
 
 type PromoListResponse = ListResponse[Promotion]
