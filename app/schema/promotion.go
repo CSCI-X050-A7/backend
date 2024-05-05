efd18da1058a29fed0231611a574b63c246fd439
pdp32147 <pdp32147@uga.edu>
2024-05-05 19:01:25 -0400

    modified promotion end date from string to time

--- app/schema/promotion.go
+++ app/schema/promotion.go
@@ -1,6 +1,8 @@
 package schema
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
@@ -8,7 +10,7 @@ import (
 type Promotion struct {
 	ID            uuid.UUID `json:"id" validate:"required"`
 	IsExpired     bool      `json:"is_expired" validate:"required"`
-	ExpiryDate    string    `json:"expiry_date" validate:"required,lte=255"`
+	ExpiryDate    time.Time `json:"expiry_date" validate:"required,lte=255"`
 	MovieAffected string    `json:"movie_affected" validate:"required,lte=255"`
 	Discount      float64   `json:"discount" validate:"required"`
 	Title         string    `json:"title" validate:"required,lte=255"`
@@ -16,12 +18,12 @@ type Promotion struct {
 }
 
 type UpsertPromotion struct {
-	IsExpired     bool    `json:"is_expired" validate:"required"`
-	ExpiryDate    string  `json:"expiry_date" validate:"required,lte=255"`
-	MovieAffected string  `json:"movie_affected" validate:"required,lte=255"`
-	Discount      float64 `json:"discount" validate:"required"`
-	Title         string  `json:"title" validate:"required,lte=255"`
-	Description   string  `json:"description" validate:"required,lte=255"`
+	IsExpired     bool      `json:"is_expired" validate:"required"`
+	ExpiryDate    time.Time `json:"expiry_date" validate:"required,lte=255"`
+	MovieAffected string    `json:"movie_affected" validate:"required,lte=255"`
+	Discount      float64   `json:"discount" validate:"required"`
+	Title         string    `json:"title" validate:"required,lte=255"`
+	Description   string    `json:"description" validate:"required,lte=255"`
 }
 
 type PromoListResponse = ListResponse[Promotion]
