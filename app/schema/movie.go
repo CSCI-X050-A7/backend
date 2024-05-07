75fead3cf7780d134a4dbf10cb15e6e4419b7190
pdp32147 <99296442+pdp32147@users.noreply.github.com>
2024-05-07 15:57:06 -0400

    fix: remove length minimums for some create movie fields (#57)
    Co-authored-by: BoYanZh <bz14520@uga.edu>

--- app/schema/movie.go
+++ app/schema/movie.go
@@ -16,8 +16,8 @@ type Movie struct {
 	Producer       string    `json:"producer" validate:"required,lte=255"`
 	Synopsis       string    `json:"synopsis" validate:"required,lte=255"`
 	Reviews        string    `json:"reviews" validate:"required,lte=255"`
-	TrailerPicture string    `json:"trailer_picture" validate:"required,lte=1023"`
-	TrailerVideo   string    `json:"trailer_video" validate:"required,lte=1023"`
+	TrailerPicture string    `json:"trailer_picture" validate:"required"`
+	TrailerVideo   string    `json:"trailer_video" validate:"required"`
 	RatingCode     string    `json:"rating_code" validate:"required,lte=255"`
 	ShowTime       time.Time `json:"show_time" validate:"required"`
 }
@@ -28,10 +28,10 @@ type UpsertMovie struct {
 	Cast           string    `json:"cast" validate:"required,lte=255"`
 	Director       string    `json:"director" validate:"required,lte=255"`
 	Producer       string    `json:"producer" validate:"required,lte=255"`
-	Synopsis       string    `json:"synopsis" validate:"required,lte=255"`
-	Reviews        string    `json:"reviews" validate:"required,lte=255"`
-	TrailerPicture string    `json:"trailer_picture" validate:"required,lte=1023"`
-	TrailerVideo   string    `json:"trailer_video" validate:"required,lte=1023"`
+	Synopsis       string    `json:"synopsis" validate:"required"`
+	Reviews        string    `json:"reviews" validate:"required"`
+	TrailerPicture string    `json:"trailer_picture" validate:"required"`
+	TrailerVideo   string    `json:"trailer_video" validate:"required"`
 	RatingCode     string    `json:"rating_code" validate:"required,lte=255"`
 	ShowTime       time.Time `json:"show_time" validate:"required"`
 }
